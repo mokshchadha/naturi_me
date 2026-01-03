08e186b34dad47cc189ebd33bdbd28a1b5daf9c0
Moksh Chadha <mokshchadha@Mokshs-MacBook-Pro.local>
2026-01-03 21:57:05 +0530

    added vercel json

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -2,10 +2,6 @@
 import posthog from 'posthog-js'
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: '/ingest', // Use your reverse proxy
-  ui_host: 'https://us.posthog.com',
-  defaults: '2025-11-30',
-  loaded: (posthog) => {
-    if (process.env.NODE_ENV === 'development') posthog.debug()
-  }
-});
+  api_host: '/ph',
+  ui_host: 'https://us.posthog.com'
+})
